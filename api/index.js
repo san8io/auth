6d0c05a67f0ae9ed626c614e5a4b4dfa99599323
santechx <santiagogf11@gmail.com>
2022-05-28 17:23:28 -0500

    feat(security): jwt tokens issuing and other security related configurations

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
+require("dotenv").config({ silent: true });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { expressjwt: jwt } = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const request = require("request");
 
 const app = express();
 
@@ -10,47 +16,96 @@ function error(status, msg) {
   return err;
 }
 
-app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
 
 app.use(function (req, res, next) {
-  var key = req.get("api-key");
+  var key = req.get("X-Parse-REST-API-Key");
 
   // key isn't present
-  if (!key) return next(error(400, "api key required"));
+  if (!key) return next(error(400, "ERROR"));
 
   // key is invalid
-  if (apiKeys.indexOf(key) === -1) return next(error(401, "invalid api key"));
+  if (apiKeys.indexOf(key) === -1) return next(error(401, "ERROR"));
 
-  // all good, store req.key for route access
-  req.key = key;
   next();
 });
 
-var apiKeys = ["foo", "bar", "baz"];
+var apiKeys = [process.env.API_KEY];
 
-app.get("/api", function (req, res, next) {
-  res.status(200);
-  res.send({ message: "OK" });
+app.get("/JWT", function (req, res, next) {
+  var options = {
+    method: "POST",
+    url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+    headers: { "content-type": "application/json" },
+    body: `{"client_id":"${process.env.AUTH0_CLIENT_ID}","client_secret":"${process.env.AUTH0_CLIENT_SECRET}","audience":"${process.env.API_IDENTIFIER}","grant_type":"client_credentials"}`,
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    const { access_token } = JSON.parse(body);
+    res.send(access_token);
+  });
 });
 
-app.post("/api", function (req, res, next) {
-  var name = req.body.name;
+var jwtUsed = [];
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 1,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  getToken: function fromCustomHeader(req) {
+    var jwtHeader = req.get("X-JWT-KWY");
+    if (jwtHeader) return jwtHeader;
+    return null;
+  },
+  // Validate the audience and the issuer.
+  audience: process.env.API_IDENTIFIER,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"],
+});
+
+app.use(checkJwt);
+app.use(function (req, res, next) {
+  var token = req.get("X-JWT-KWY");
+
+  // jwt token already used
+  if (jwtUsed.indexOf(token) !== -1) {
+    return next(error(401, "ERROR"));
+  } else {
+    jwtUsed.push(token);
+  }
+
+  next();
+});
+
+app.post("/DevOps", function (req, res, next) {
+  var to = req.body.to;
   res.status(200);
-  res.send({ name: name });
+  res.send({ message: `Hello ${to} your message will be send` });
+});
+
+app.all("/DevOps", function (req, res) {
+  res.status(405);
+  res.send("ERROR");
 });
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.send({ error: err.message });
+  console.log(err.message);
+  res.send("ERROR");
 });
 
 app.use(function (req, res) {
   res.status(404);
-  res.send({ error: "Sorry, can't find that" });
+  res.send("ERROR");
 });
 
-if (!module.parent) {
-  app.listen(3000);
-  console.log("Express started on port 3000");
-}
+app.listen(process.env.PORT, () => {
+  console.log(`Server Listening on port ${process.env.PORT}`);
+});
